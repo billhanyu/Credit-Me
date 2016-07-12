354eaf1d8f603c56dd6d20a0ff9c1bc2fcb912c0
Bill Yu <han.yu@duke.edu>
2016-07-11 20:07:10 -0400

    Initial Commit

---
+++ Credit-Me/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Credit-Me
+//
+//  Created by Bill Yu on 7/11/16.
+//  Copyright © 2016 Bill Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
